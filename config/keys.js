83b1e559319ec0afc38f924789b3cf93e4bfdc65
Aman Sachdev <aman.sachdev@cleardata.com>
2022-08-27 13:18:15 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://chat:7T0suyy3YPwjKBbV@cluster0.gqpayy9.mongodb.net/?retryWrites=true&w=majority",
+  secretOrKey: require('crypto').randomBytes(256).toString('base64')
+};
